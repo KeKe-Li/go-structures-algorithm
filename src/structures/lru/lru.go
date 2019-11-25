b508d0e9dc8aff9a329903f8ba55a8b42be0adcc
lisensen <lisensen@innotechx.com>
2019-11-25 16:05:55 +0800

    feat: skiplist

---
+++ src/structures/lru/lru.go
@@ -0,0 +1,93 @@
+package lru
+
+// LruCache 基于内存实现、不带过期时间
+// 原理：map结构按照kv存储数据，双向链表保存数据新鲜度
+// 扩展：支持过期时间可以增加一个双向链表按过期时间存储，
+
+// LRUChainNode 链表节点
+type LRUChainNode struct {
+	pre   *LRUChainNode
+	next  *LRUChainNode
+	key   int
+	value int
+	ts    int32
+}
+
+// LRUCache 结构
+type LRUCache struct {
+	capacity int
+	length   int
+	store    map[int]*LRUChainNode
+	head     *LRUChainNode
+	tail     *LRUChainNode
+}
+
+// NewLRUCache constructor
+func NewLRUCache(capacity int) LRUCache {
+	return LRUCache{
+		capacity: capacity,
+		length:   0,
+		store:    map[int]*LRUChainNode{},
+	}
+}
+
+// Delete 删除key
+func (c *LRUCache) Delete(key int) {
+	node, exist := c.store[key]
+	if !exist {
+		return
+	}
+	delete(c.store, key)
+	if c.length == 1 {
+		c.head = nil
+		c.tail = nil
+		c.length--
+		return
+	}
+	if node.pre == nil {
+		c.head = c.head.next
+		c.head.pre = nil
+	} else {
+		node.pre.next = node.next
+	}
+	if node.next == nil {
+		c.tail = c.tail.pre
+		c.tail.next = nil
+	} else {
+		node.next.pre = node.pre
+	}
+	c.length--
+}
+
+// Get 获取kv
+func (c *LRUCache) Get(key int) int {
+	node, exist := c.store[key]
+	if !exist {
+		return -1
+	}
+	c.Delete(key)
+	c.Put(node.key, node.value)
+	return node.value
+}
+
+// Put 插入
+func (c *LRUCache) Put(key int, value int) {
+	c.Delete(key)
+	if c.length+1 > c.capacity {
+		c.Delete(c.tail.key)
+	}
+	node := LRUChainNode{key: key, value: value}
+	if c.length == 0 {
+		c.head = &node
+		c.tail = &node
+		c.store[key] = &node
+		c.length++
+		return
+	}
+	// 头部处理
+	c.head.pre = &node
+	node.next = c.head
+	c.head = &node
+	c.store[key] = &node
+	c.length++
+}
---
+++ src/structures/lru/lru_test.go
@@ -0,0 +1,29 @@
+package lru
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLru(t *testing.T) {
+	cache := NewLRUCache(1)
+	cache.Put(1, 11)
+	cache.Put(2, 22)
+	cache.Put(3, 33)
+	fmt.Println("put success")
+
+	v := cache.Get(2)
+	fmt.Printf("get (2) return %v\n", v) // 返回  22
+
+	v = cache.Get(2)
+	fmt.Printf("get (2) return %v\n", v) // 返回  22
+
+	cache.Put(4, 44) // 该操作会使(2,22) 作废
+	fmt.Println("put 4 success")
+
+	v = cache.Get(2)
+	fmt.Printf("get (2) return %v \n", v) // 返回 -1 (未找到)
+
+	v = cache.Get(3)
+	fmt.Printf("get (3) return %v\n", v) // 返回  33
+}
